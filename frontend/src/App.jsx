2b3e268bd358902b615e5b81f5f0e5865c8c43a1
Wilson Alzuguir <walzuguir@gmail.com>
2026-06-24 09:17:33 -0300

    feat: routes protection with PrivateRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import EntradaInsumos from "./pages/EntradaInsumos";
 import CadastroGestor from "./pages/CadastroGestor";
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/entrada" element={<EntradaInsumos />} />
-        <Route path="/gestor" element={<CadastroGestor />} />
+        <Route path="/entrada" element={<PrivateRoute>
+          <EntradaInsumos /></PrivateRoute>} />
+        <Route path="/gestor" element={<PrivateRoute>
+          <CadastroGestor /></PrivateRoute>
+        } />
       </Routes>
     </BrowserRouter>
   );
